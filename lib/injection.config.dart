29cf6dd440f367cf2dae10d727b9170ac4fc8503
Jose Egito Silva <jegitosi@emeal.nttdata.com>
2023-10-26 11:36:33 -0300

    bloc creation

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -12,14 +12,20 @@ import 'package:dio/dio.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:ricky_n_morty_aap/characters/data/repository/character_repository.dart'
-    as _i5;
+    as _i8;
 import 'package:ricky_n_morty_aap/characters/data/service/character_service.dart'
-    as _i4;
-import 'package:ricky_n_morty_aap/characters/domain/character_interactor.dart'
     as _i6;
+import 'package:ricky_n_morty_aap/characters/domain/character_interactor.dart'
+    as _i9;
 import 'package:ricky_n_morty_aap/characters/presentation/bloc/character_bloc.dart'
+    as _i10;
+import 'package:ricky_n_morty_aap/details/data/repository/details_repository.dart'
+    as _i5;
+import 'package:ricky_n_morty_aap/details/data/service/character_detail_service.dart'
+    as _i4;
+import 'package:ricky_n_morty_aap/details/presentation/bloc/character_details_bloc.dart'
     as _i7;
-import 'package:ricky_n_morty_aap/network/network_module.dart' as _i8;
+import 'package:ricky_n_morty_aap/network/network_module.dart' as _i11;
 
 // initializes the registration of main-scope dependencies inside of GetIt
 _i1.GetIt init(
@@ -45,15 +51,21 @@ _i1.GetIt init(
     gh<String>(instanceName: 'BaseUrl'),
     gh<_i3.Interceptor>(instanceName: 'PrettyLogger'),
   ));
-  gh.factory<_i4.CharacterService>(
-      () => _i4.CharacterServiceImpl(gh<_i3.Dio>()));
-  gh.factory<_i5.CharacterRepository>(
-      () => _i5.CharactersRepositoryImpl(gh<_i4.CharacterService>()));
-  gh.factory<_i6.CharactersInteractor>(
-      () => _i6.CharactersInteractorImpl(gh<_i5.CharacterRepository>()));
-  gh.factory<_i7.CharacterBloc>(
-      () => _i7.CharacterBloc(gh<_i6.CharactersInteractor>()));
+  gh.factory<_i4.CharacterDetailService>(
+      () => _i4.CharacterDetailServiceImpl(gh<_i3.Dio>()));
+  gh.factory<_i5.CharacterDetailsRepository>(() =>
+      _i5.CharacterDetailsRepositoryImpl(gh<_i4.CharacterDetailService>()));
+  gh.factory<_i6.CharacterService>(
+      () => _i6.CharacterServiceImpl(gh<_i3.Dio>()));
+  gh.factory<_i7.CharacterDetailsBloc>(
+      () => _i7.CharacterDetailsBloc(gh<_i5.CharacterDetailsRepository>()));
+  gh.factory<_i8.CharacterRepository>(
+      () => _i8.CharactersRepositoryImpl(gh<_i6.CharacterService>()));
+  gh.factory<_i9.CharactersInteractor>(
+      () => _i9.CharactersInteractorImpl(gh<_i8.CharacterRepository>()));
+  gh.factory<_i10.CharacterBloc>(
+      () => _i10.CharacterBloc(gh<_i9.CharactersInteractor>()));
   return getIt;
 }
 
-class _$NetworkModule extends _i8.NetworkModule {}
+class _$NetworkModule extends _i11.NetworkModule {}
